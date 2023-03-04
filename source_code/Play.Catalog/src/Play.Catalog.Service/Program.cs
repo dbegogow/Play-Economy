c75886d6bf62c618c09b7a51f5b531506a559f3c
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-03-04 23:24:26 +0200

    Refactoring MongoDB registration into extension methods

--- source_code/Play.Catalog/src/Play.Catalog.Service/Program.cs
+++ source_code/Play.Catalog/src/Play.Catalog.Service/Program.cs
@@ -1,35 +1,12 @@
-using Play.Catalog.Service.Settings;
-using Play.Catalog.Service.Repositories;
 using Play.Catalog.Service.Entities;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Serializers;
-using MongoDB.Driver;
+using Play.Catalog.Service.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-
 var configuration = builder.Configuration;
 
-var serviceSettings = configuration
-    .GetSection(nameof(ServiceSettings))
-    .Get<ServiceSettings>();
-
-builder.Services.AddSingleton(serviceProvider =>
-{
-    var mongoDbSettings = configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-    var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-
-    return mongoClient.GetDatabase(serviceSettings.ServiceName);
-});
-
-builder.Services.AddSingleton<IRepository<Item>>(serviceProvider =>
-{
-    var database = serviceProvider.GetService<IMongoDatabase>();
-    return new MongoRepository<Item>(database, "items");
-});
+builder.Services.AddMongo(configuration);
+builder.Services.AddMongoRepository<Item>("items");
 
 builder.Services.AddControllers(options =>
 {
---
+++ source_code/Play.Catalog/src/Play.Catalog.Service/Repositories/Extensions.cs
@@ -0,0 +1,46 @@
+﻿using Play.Catalog.Service.Entities;
+using Play.Catalog.Service.Settings;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Play.Catalog.Service.Repositories;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMongo(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+        BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+        var serviceSettings = configuration
+            .GetSection(nameof(ServiceSettings))
+            .Get<ServiceSettings>();
+
+        services.AddSingleton(serviceProvider =>
+        {
+            var mongoDbSettings = configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+            var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
+
+            return mongoClient.GetDatabase(serviceSettings.ServiceName);
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddMongoRepository<T>(
+        this IServiceCollection services,
+        string collectionName) where T : IEntity
+    {
+        services.AddSingleton<IRepository<T>>(serviceProvider =>
+        {
+            var database = serviceProvider.GetService<IMongoDatabase>();
+            return new MongoRepository<T>(database, collectionName);
+        });
+
+        return services;
+    }
+}
