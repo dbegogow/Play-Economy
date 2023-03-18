590606ffac8adfbaf930c72776595eaf24a82154
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-03-18 16:33:41 +0200

    Implementing the circuit breaker pattern

--- source_code/Play.Inventory/src/Play.Inventory.Service/Program.cs
+++ source_code/Play.Inventory/src/Play.Inventory.Service/Program.cs
@@ -3,6 +3,7 @@ using Play.Inventory.Service.Clients;
 using Play.Common.MongoDB;
 using Polly;
 using Polly.Timeout;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,13 +18,28 @@ builder.Services
     .AddHttpClient<CatalogClient>(client
         => client.BaseAddress = new Uri("https://localhost:7281"))
     .AddTransientHttpErrorPolicy(policyBuilder
-        => policyBuilder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
-            5,
-            retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(random.Next(0, 1000)),
-            onRetry: (outcome, timespan, retryAttempt) =>
-                Console.WriteLine(
-                    $"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}")
-            ))
+        => policyBuilder
+            .Or<TimeoutRejectedException>()
+            .WaitAndRetryAsync(
+                5,
+                retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(random.Next(0, 1000)),
+                onRetry: (outcome, timespan, retryAttempt) =>
+                    Console.WriteLine(
+                        $"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}")
+                ))
+    .AddTransientHttpErrorPolicy(policyBuilder
+        => policyBuilder
+            .Or<TimeoutRejectedException>()
+            .CircuitBreakerAsync(
+                3,
+                TimeSpan.FromSeconds(15),
+                onBreak: (outcome, timespan) =>
+                    Console.WriteLine(
+                        $"Opening the circuit for {timespan.TotalSeconds} seconds..."),
+                onReset: () =>
+                    Console.WriteLine(
+                        $"Closing the circuit...")
+                ))
     .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
 builder.Services.AddControllers();
