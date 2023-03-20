2b99cb086ea6af45eaf04afa0e053626da2c213a
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-03-20 08:25:42 +0200

    Implement CatalogItemCreatedConsumer in Inventory service

---
+++ source_code/Play.Inventory/src/Play.Inventory.Service/Consumers/CatalogItemCreatedConsumers.cs
@@ -0,0 +1,36 @@
+﻿using Play.Inventory.Service.Entities;
+using Play.Common;
+using Play.Catalog.Contracts;
+using MassTransit;
+
+namespace Play.Inventory.Service.Consumers;
+
+public class CatalogItemCreatedConsumers : IConsumer<CatalogItemCreated>
+{
+    private readonly IRepository<CatalogItem> repository;
+
+    public CatalogItemCreatedConsumers(IRepository<CatalogItem> repository)
+        => this.repository = repository;
+
+    public async Task Consume(ConsumeContext<CatalogItemCreated> context)
+    {
+        var message = context.Message;
+
+        var item = await this.repository
+            .GetAsync(message.ItemId);
+
+        if (item != null)
+        {
+            return;
+        }
+
+        item = new CatalogItem
+        {
+            Id = message.ItemId,
+            Name = message.Name,
+            Description = message.Description
+        };
+
+        await repository.CreateAsync(item);
+    }
+}
