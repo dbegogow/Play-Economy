6d2c0e09bf39b6cca0e233e202f06839f9d3c6e5
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-03-20 08:34:00 +0200

    Implement CatalogItemDeletedConsumer in Inventory service

---
+++ source_code/Play.Inventory/src/Play.Inventory.Service/Consumers/CatalogItemDeletedConsumers.cs
@@ -0,0 +1,30 @@
+﻿using Play.Inventory.Service.Entities;
+using Play.Common;
+using Play.Catalog.Contracts;
+using MassTransit;
+
+namespace Play.Inventory.Service.Consumers;
+
+public class CatalogItemDeletedConsumers : IConsumer<CatalogItemDeleted>
+{
+    private readonly IRepository<CatalogItem> repository;
+
+    public CatalogItemDeletedConsumers(IRepository<CatalogItem> repository)
+        => this.repository = repository;
+
+    public async Task Consume(ConsumeContext<CatalogItemDeleted> context)
+    {
+        var message = context.Message;
+
+        var item = await this.repository
+            .GetAsync(message.ItemId);
+
+        if (item == null)
+        {
+            return;
+        }
+
+        await this.repository
+            .RemoveAsync(message.ItemId);
+    }
+}
